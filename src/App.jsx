b5a0b289eb7947e91f72ba09616b8f1a28cf10b9
Eric Castro <30558767+EricCastr0@users.noreply.github.com>
2022-11-29 16:22:27 -0300

    4 att

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
-import React, {useState, useEffect}from 'react';
-import {supabase} from "./components/connection/Connection";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home/index'
 import Products from './components/pages/Products/index'
--- src/components/pages/Products/index.jsx
+++ src/components/pages/Products/index.jsx
@@ -1,8 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect }from 'react'
 import {supabase} from "../../connection/Connection";
 import './styles.css';
+import { AiOutlineEdit, AiOutlineDelete} from "react-icons/ai";
+import { Button } from 'antd';
+import Modal from "react-modal";
 
-const index = () => {
+Modal.setAppElement("#root");
+
+function index() {
+  const [modalIsOpem, setIsOpem] = useState(false);
+
+  function opemModal(){
+      setIsOpem(true);
+  }
+
+  function closeModal(){
+      setIsOpem(false);
+  }
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      padding: '200px'
+    }
+  }
+
   const [produtos, setProdutos]= useState([])
   // console.log(produtos);
 
@@ -19,21 +46,48 @@ const index = () => {
   }
   return (
     <>
-    <table>
-        <tr>
-          <th>Id</th>
-          <th>Produto</th>
-          <th>Preço</th>
-        </tr>
-      {produtos.length > 0 && produtos.map((produto) => (
-        <tr key={Math.random().toString()}>
-          <td>{produto?.id}</td>
-          <td>{produto?.name}</td>
-          <td>{produto?.price}</td>
-        </tr>
-        
-      ))}
-      </table>
+    <table className='tabela'>
+      <tbody>
+      <Button type='primary' className='btnNew' onClick={opemModal}>
+        Novo Produto
+      </Button>
+      <Modal
+        isOpen={modalIsOpem}
+        onRequestClose={closeModal}
+        style={customStyles}
+        >
+        <div className='formProd'>
+          <h1>form</h1>
+          <Button type='primary' danger  onClick={closeModal}>Cancelar</Button>
+          <form action="">
+            <input/>
+          </form>
+        </div>
+      </Modal>
+          <tr>
+            <th className='fun'></th>
+            <th>Id</th>
+            <th>Produto</th>
+            <th>Preço</th>
+            <th>Estoque</th>
+
+          </tr>
+        {produtos.length > 0 && produtos.map((produto) => (
+          <tr key={Math.random().toString()}>
+            <td className='edit'>
+            <button className='btnEdit'>{<AiOutlineEdit/>}</button> 
+            <button className='btnDelete'>{<AiOutlineDelete/>}</button>
+            </td>
+            
+            <td>{produto?.id}</td>
+            <td>{produto?.name}</td>
+            <td>{produto?.price}</td>
+            <td>{produto?.stock}</td>
+          </tr>
+          
+        ))}
+      </tbody>
+    </table>
     </>
   )
 }
